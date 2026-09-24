bf333369ee1220ed15cfff8e6e86613695c7bda3
Thais <thais.felix@etudiant.univ-reims.fr>
2026-09-24 23:37:17 +0200

    fix: nettoyage de la configuration vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,15 +2,6 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
   test: {
-    environment: 'nuxt',
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html'],
-      include: ['utils/promotions.ts'],
-      thresholds: {
-        lines: 90,
-        branches: 90
-      }
-    }
+    environment: 'nuxt'
   }
 })
